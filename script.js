d02f0b12368dfb8477d32c1551b5de1a5bc0b444
Milton Cruz <mr.cruzm@gmail.com>
2026-04-09 10:26:36 -0700

    Variables and Calculator Snippets

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello from my About Me page!");
+
+    document.getElementById('calculate').addEventListener('click', function() {
+
+            // Get the values from the input fields
+            let inputNum1 = document.getElementById('num1');
+            console.log("inputNum1", inputNum1);
+            let num1 = Number(inputNum1.value);
+
+
+            let num2 = Number(document.getElementById('num2').value);
+            let sum = num1 + num2;
+
+            let results = document.getElementById('result');
+            console.log("results", results);
+            results.innerHTML = "Sum: " + sum;
+
+
+            console.log("sum", sum);
+    });
 });
