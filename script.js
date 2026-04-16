5873fac144e98b0a75218e17a0f6a9799864ef94
Milton Cruz <mr.cruzm@gmail.com>
2026-04-16 10:01:42 -0700

    Added function for Guess Number

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+function loadGuessNumber() {
+
+    // Generate a random number between 1 and 10
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    console.log("randomNumber", randomNumber);
+    let attempts = 0;
+
+    // Initialize the guess variable
+    let guess;
+
+    while (guess !== randomNumber) {
+
+        // Prompt the user to guess a number
+        guess = Number(prompt("Guess a number between 1 and 10:"));
+
+        // Increment the attempts counter
+        attempts++;
+
+        // Check if the guess is correct
+        if (guess > randomNumber) {
+            console.log("Too high! Try again.");
+        }
+        else if (guess < randomNumber) {
+            console.log("Too low! Try again.");
+        }
+        else {
+           console.log(`Congratulations! You guessed the number in ${attempts} attempts. The number was ${randomNumber}.`);
+        }
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('calculate').addEventListener('click', function() {
+    // LOAD guesss number
+    loadGuessNumber();
+
+    let calButton = document.getElementById('calculate');
+
+    if (calButton) {
+        calButton.addEventListener('click', function() {
 
             // Get the values from the input fields
             let inputNum1 = document.getElementById('num1');
@@ -17,5 +55,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
             console.log("sum", sum);
-    });
+        });
+    }
 });
